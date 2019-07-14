814198035c774ddb69fd915bd104d7d7bef7afd3
John Brennan <jgabrennan@gmail.com>
2019-07-14 13:35:00 +0100

    clean up of some files

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#750a5c`,
       marginBottom: `1.45rem`,
     }}
   >
